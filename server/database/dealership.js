5d30dd78a3b102126eea8e1d421d1c3e1af61e12
SiRipo92 <ripochesierra@gmail.com>
2025-01-09 11:45:00 -0500

    Reverted file back to linted version

--- server/database/dealership.js
+++ server/database/dealership.js
@@ -1,11 +1,14 @@
+/* jshint esversion: 6 */
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
+
 const dealerships = new Schema({
-	id: {
+	    id: {
     type: Number,
     required: true,
 	},
-	city: {
+	    city: {
     type: String,
     required: true
   },
@@ -37,4 +40,5 @@ const dealerships = new Schema({
     required: true
   }
 });
+
 module.exports = mongoose.model('dealerships', dealerships);

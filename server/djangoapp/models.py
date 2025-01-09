daaa7181d81602fa47d4ac9b78150dfdbc1b5b11
SiRipo92 <ripochesierra@gmail.com>
2025-01-09 08:23:27 -0500

    Linting corrections to models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -43,11 +43,13 @@ class CarModel(models.Model):
         ('PICKUP', 'Pickup'),
     ]
     type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2025),
-            MinValueValidator(2015)
-        ])
-    
+            MinValueValidator(2015),
+        ]
+    )
+
     def __str__(self):
-        return self.name  
+        return self.name

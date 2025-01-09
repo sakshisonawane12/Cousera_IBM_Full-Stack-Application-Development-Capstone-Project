1796b2f246687ed0459da036e71c85526ac3bb23
SiRipo92 <ripochesierra@gmail.com>
2025-01-09 06:27:00 -0500

    Added React Dealer component that shows reviews

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -104,18 +104,28 @@ def get_dealerships(request, state="All"):
     return JsonResponse({ "status":200, "dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(request,dealer_id):
+def get_dealer_reviews(request, dealer_id):
     # If dealer id has been provided
-    if (dealer_id):
+    if dealer_id:
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
+        
         for review_detail in reviews:
+            # Call analyze_review_sentiments and check if the response is valid
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
-            review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status":200, "reviews": reviews})
+            
+            # Check if the response is not None and contains the 'sentiment' key
+            if response is not None and 'sentiment' in response:
+                review_detail['sentiment'] = response['sentiment']
+            else:
+                # If there's no sentiment or the response is None, default to 'neutral'
+                review_detail['sentiment'] = 'neutral'
+
+        return JsonResponse({"status": 200, "reviews": reviews})
+    
     else:
-        return JsonResponse({"status":400, "message": "Bad Request"})
+        return JsonResponse({"status": 400, "message": "Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import LoginPanel from "./components/Login/Login"; // Importing the Login component
 import Register from "./components/Register/Register"; // Importing the Register components
 import Dealers from './components/Dealers/Dealers';
+import Dealer from './components/Dealers/Dealer';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         {/* Route for the Register page */}
         <Route path="/register" element={<Register />} />
         <Route path="/dealers" element={<Dealers/>} />
+        <Route path="/dealer/:id" element={<Dealer/>} />
     </Routes>
   );
 }
